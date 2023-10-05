504d6eebaebf8f84a9d4b426fa0c89fcdebefee8
ALoNePoMaHTiK <pomahtik11@gmail.com>
2023-10-05 11:40:53 +0300

    Добавьте файлы проекта.

---
+++ TechnogenicSecurity/Models/FireParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnogenicSecurity.Models
+{
+    public class FireParameter
+    {
+        public double Distance { get; set; }
+        public double IncidentHeatFluxDensity { get; set; }
+        public double ProbitFunctionValue { get; set; }
+    }
+}
